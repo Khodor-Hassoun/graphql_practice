9396d26393d6f5c27c04053167567602abedd1c8
Khodor Hassoun <khodorhassoun@outlook.com>
2022-12-25 21:13:01 +0200

    Design graphql schema

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,9 +1,10 @@
 import { createServer } from "node:http";
+import { schema } from "./schema";
 import { createYoga } from "graphql-yoga";
 require("dotenv").config();
 const port = Number(process.env.API_PORT);
 
-const yoga = createYoga({});
+const yoga = createYoga({ schema });
 const server = createServer(yoga);
 server.listen(port, () => {
   console.info(`Server is running on http://localhost:4000/graphql ${port}`);
