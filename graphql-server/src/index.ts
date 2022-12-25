e2c3ad9275940dff4bb20723617eb72bdb2675d6
Khodor Hassoun <khodorhassoun@outlook.com>
2022-12-25 19:21:47 +0200

    Start graphql server

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hi");
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+require("dotenv").config();
+const port = Number(process.env.API_PORT);
+
+const yoga = createYoga({});
+const server = createServer(yoga);
+server.listen(port, () => {
+  console.info(`Server is running on http://localhost:4000/graphql ${port}`);
+});
